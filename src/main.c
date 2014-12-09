d5e4eb02b1d8eb78eda03453dbd4ec7cd6e6ff65
Vadorequest <ambroise.dhenain@gmail.com>
2014-12-10 00:06:35 +0100

    Compile...

---
+++ src/main.c
@@ -0,0 +1,15 @@
+/*
+ ============================================================================
+ Name        : Projet-C-Exia-2014.c
+ Author      : Vadorequest
+ Copyright   : MIT
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Coucou"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
