b13ec8c65b5090e116e94fdcbb4878a1b65e9f7a
Vadorequest <ambroise.dhenain@gmail.com>
2014-12-12 23:21:22 +0100

    Change location of files.

--- src/history.h
+++
@@ -1,8 +0,0 @@
-#ifndef HISTORY_H
-#define HISTORY_H
-
-void display_history();
-void add_new_entry();
-
-
-#endif
--- src/menu.h
+++
@@ -1,8 +0,0 @@
-#ifndef MENU_H
-#define MENU_H
-
-void display_menu();
-void display_level();
-
-
-#endif
